577d686efe1b6777d559de964489cd7b90620222
Oswaldo de Jesús Chacón Pérez <39079647+OswaldoChacon@users.noreply.github.com>
2022-05-29 20:41:25 -0500

    Started project

--- database/migrations/2022_05_30_012636_create_dates_table.php
+++ database/migrations/2022_05_30_012636_create_dates_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('dates', function (Blueprint $table) {
+            $table->id();
+            $table->date('date')->unique();
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('dates');
     }
 };
